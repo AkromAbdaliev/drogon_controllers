c8e346ec94a8b64e973ab10b56a460f8577cfb65
AkromAbdaliev <akromjon996@gmail.com>
2025-07-22 09:47:22 +0500

    first commit

---
+++ build/CMakeFiles/drogon_controllers.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drogon_controllers.dir/main.cc.o"
+  "CMakeFiles/drogon_controllers.dir/main.cc.o.d"
+  "drogon_controllers"
+  "drogon_controllers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drogon_controllers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/test/CMakeFiles/drogon_controllers_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drogon_controllers_test.dir/test_main.cc.o"
+  "CMakeFiles/drogon_controllers_test.dir/test_main.cc.o.d"
+  "drogon_controllers_test"
+  "drogon_controllers_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drogon_controllers_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
